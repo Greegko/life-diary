a86fd4a272c406ce36765acd789f43464765fc98
Greegko <greegko+dev@gmail.com>
2021-01-10 17:09:59 +0100

    Add tracking option

--- src/app/components/pages/page-track.tsx
+++ src/app/components/pages/page-track.tsx
@@ -1,9 +1,13 @@
-import React, { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
-import { Page, pageStateAtom } from '../app.state';
+import React, { useEffect, useState } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { Goal, GoalTrack } from '../../store';
+import { activeGoalsAtom, Page, pageStateAtom } from '../app.state';
+import { ScreenOverlay } from '../common/screen-overlay';
 
+import './page-track.scss';
 export const PageTrack = () => {
   const setPage = useSetRecoilState(pageStateAtom);
+  const activeGoals = useRecoilValue(activeGoalsAtom);
 
   useEffect(() => {
     setPage({
@@ -16,5 +20,38 @@ export const PageTrack = () => {
     });
   }, []);
 
-  return <div>Page Track</div>;
+  return (
+    <div className="page-track">
+      <div>Page Track</div>
+      <div>Goals:</div>
+      <div>
+        {activeGoals.map(goal => <Goal goal={goal} />)}
+      </div>
+    </div>
+  );
+}
+
+const Goal = ({ goal }: { goal: Goal }) => (
+  <div className="goal">
+    <div className="goal__title">{goal.name}</div>
+    {(goal.tracks || []).map(track => <GoalTrackAction track={track} />)}
+  </div>
+);
+
+const GoalTrackAction = ({ track }: { track: GoalTrack }) => {
+  const [overlay, setOverlay] = useState<boolean>();
+
+  return (
+    <>
+      {overlay && (
+        <ScreenOverlay onClose={() => setOverlay(false)}>
+          Config
+        </ScreenOverlay>
+      )}
+      <span className="goal-track" onClick={() => setOverlay(true)}>
+
+        {track.type}
+      </span>
+    </>
+  );
 }
